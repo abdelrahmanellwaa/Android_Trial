cd10925e2becad763e8139bd44e8e498479a54ac
abdelrahman <abdelrahman.ellewaa@blink22.com>
2017-01-03 13:13:53 +0200

    adding a violation method b51

--- app/src/main/java/infer/other/MainActivity.java
+++ app/src/main/java/infer/other/MainActivity.java
@@ -33,6 +33,10 @@ protected void onCreate_222222222222(Bundle savedInstanceState) {
   }
 
 
+  protected void method_violation(Bundle savedInstanceState) {
+    source().toString();
+  }
+
   @SuppressWarnings("infer")
   void shouldNotBeReported() {
     source().toString();
